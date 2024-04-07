6389752aade2443fbf794dfce0812f10b30a9b8e
Vladyslav <vladbuldenko@gmail.com>
2024-04-07 11:20:37 +0300

    VB Person solution mod

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,16 @@
 export const Person = ({ person }) => {
+  const partnerType = person.sex === 'm' ? 'wife' : 'husband';
+  const partner = person.isMarried
+    ? `${person.partnerName} is my ${partnerType}`
+    : 'I am not married';
+
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
 
       {person.age && <p className="Person__age">I am {person.age}</p>}
 
-      {person.isMarried ? (
-        <p className="Person__partner">
-          {person.partnerName} is my {person.sex === 'f' ? 'husband' : 'wife'}
-        </p>
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
+      <p className="Person__partner">{partner}</p>
     </section>
   );
 };
